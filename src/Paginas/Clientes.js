acdccb491765c76f3d4b83dfcb8591eee4f3bd70
Blucilhã <blucilha.nascimento@gmail.com>
2021-10-06 11:59:36 -0300

    Constrói Tabela de clientes

--- src/Paginas/Clientes.js
+++ src/Paginas/Clientes.js
@@ -1,10 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import TableClients from "../Components/TableClients";
 import contextApi from "../ContextAPI/ContextApi";
 
 function Clientes() {
     const { clientRandom } = useContext(contextApi);
+    const [clients, setClients] = useState();
+
+    useEffect(() => {
+        const stateClients = async () => {
+            const clientRandomArray = await clientRandom;
+            setClients(clientRandomArray)
+        };
+
+        stateClients();
+    }, [clientRandom]);
+    console.log([clientRandom, clients])
     return(
-        <h1>{ `Sou o número: 1` }</h1>
+        <div>
+            <TableClients client={clients} />
+        </div>
     )
 }
 
